9e61de36965481ac55be0d8a4eea650837a9a73c
s3fs <northfacebodybags@yahoo.com>
2021-09-07 18:15:02 +0500

    conter init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from 'react-dom'
+import { createStore } from "redux"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//reducer gets curr state and desired action and returns changed state
+//never invoked directly, but rather
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCR':
+      return state + 1
+    case 'DECR':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//is invoked by methods of the store instance created like below
+const store = createStore(counterReducer)
+
+const App = () => {
+  return (
+    <div>
+      <div>{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: 'INCR' })}>increase</button>
+      <button onClick={() => store.dispatch({ type: 'DECR' })}>decrease</button>
+      <button onClick={() => store.dispatch({ type: 'ZERO' })}>to zero</button>
+    </div>
+  )
+}
+
+const logger = () => {
+  const currState = store.getState()
+  console.log(currState)
+}
+store.subscribe(logger)
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+renderApp()
+store.subscribe(renderApp)
