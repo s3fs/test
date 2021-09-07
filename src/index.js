02945473be6a1c736c6701bc9642d2a31b9a7cc7
s3fs <northfacebodybags@yahoo.com>
2021-09-07 22:45:06 +0500

    further fnty devt

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
 import React from "react"
 import ReactDOM from 'react-dom'
+import { createStore } from "redux"
 import { store } from "./reducers/noteReducer"
 
+const genId = () => {
+  Math.floor(Math.random() * 1000000)
+}
+
 const App = () => {
+  const createNote = (content) => {
+    return {
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: genId()
+      }
+    }
+  }
+  
+  const toggleImpActionCreator = (id) => {
+    return {
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+    }
+  }
+
+  const addNote = (ev) => {
+    ev.preventDefault()
+    const content = ev.target.note.value
+    ev.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImp = (id) => {
+    store.dispatch(toggleImpActionCreator(id))
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name='note' />
+        <button type='submit'>add</button>
+      </form>
       <ul>
         {store.getState().map(n => 
-          <li key={n.id}>
+          <li key={n.id} onClick={() => toggleImp(n.id)}>
             {n.content} <strong>{n.important ? 'note is important' : ''}</strong>
           </li>  
         )}
